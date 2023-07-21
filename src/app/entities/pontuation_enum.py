f59bfe5f7f2112e1c35bd20136b715b0c3930923
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-07-21 15:58:11 -0300

    Added get lowest and highest score methods on enum
    - Added get lowest score method on Points ENUM
    
    - Added get highest score method on Points ENUM

--- src/app/entities/pontuation_enum.py
+++ src/app/entities/pontuation_enum.py
@@ -6,3 +6,9 @@ class PointsEnum(Enum):
     SNAKE = -2
     HAZARD = -1
     EMPTY = 0
+
+    def get_highest_score(self):
+        return max(self.SNAKE.value, self.EMPTY.value, self.HAZARD.value, self.FOOD.value)
+
+    def get_lowest_score(self):
+        return min(self.SNAKE.value, self.EMPTY.value, self.HAZARD.value, self.FOOD.value)
