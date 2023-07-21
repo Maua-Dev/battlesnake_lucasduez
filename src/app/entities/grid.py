b68719c806f5d1b2004826bab6ddc479c9f5c355
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-07-21 12:20:28 -0300

    Made grid entity with UTs

---
+++ src/app/entities/grid.py
@@ -0,0 +1,36 @@
+import dataclasses
+
+from .pontuation_enum import PointsEnum
+
+
+@dataclasses.dataclass(init=False)
+class GridEntity:
+    rows: int
+    columns: int
+    grid_matrix: [[]]
+
+    def __init__(self, rows: int, columns: int, snakes_positions_list: [], foods_positions_list: [],
+                 hazards_positions_list: []):
+
+        self.rows = rows
+        self.columns = columns
+
+        self.update_grid_values(snakes_positions_list, foods_positions_list, hazards_positions_list)
+
+    def update_grid_values(self, snakes_positions_list: [], foods_positions_list: [],
+                           hazards_positions_list: []):
+
+        self.grid_matrix = [[PointsEnum.EMPTY.value for _ in range(self.columns)] for _ in range(self.rows)]
+
+        for i in range(max(len(snakes_positions_list), len(foods_positions_list), len(hazards_positions_list))):
+            if 0 < len(foods_positions_list) > i:
+                food_pos = foods_positions_list[i]
+                self.grid_matrix[food_pos['x']][food_pos['y']] = PointsEnum.FOOD.value
+
+            if 0 < len(hazards_positions_list) > i:
+                hazard_pos = hazards_positions_list[i]
+                self.grid_matrix[hazard_pos['x']][hazard_pos['y']] = PointsEnum.HAZARD.value
+
+            if 0 < len(snakes_positions_list) > i:
+                snake_pos = snakes_positions_list[i]
+                self.grid_matrix[snake_pos['x']][snake_pos['y']] = PointsEnum.SNAKE.value
---
+++ tests/app/entities/test_grid.py
@@ -0,0 +1,46 @@
+from src.app.entities.grid import GridEntity
+from src.app.entities.pontuation_enum import PointsEnum
+
+
+class Test_Grid:
+    def test_grid_entity_must_be_initialized_with_zeros(self):
+        grid = GridEntity(3, 3, [], [], [])
+
+        assert grid.rows == 3
+        assert grid.columns == 3
+
+        for i in range(len(grid.grid_matrix)):
+            for j in range(len(grid.grid_matrix[i])):
+                assert grid.grid_matrix[i][j] == 0
+
+    def test_grid_0_0_and_1_2_positions_must_be_snake_value(self):
+        grid = GridEntity(3, 3, snakes_positions_list=[{"x": 0, "y": 0}, {'x': 1, 'y': 2}],
+                          foods_positions_list=[],
+                          hazards_positions_list=[])
+
+        assert grid.rows == 3
+        assert grid.columns == 3
+        assert grid.grid_matrix[0][0] == PointsEnum.SNAKE.value
+        assert grid.grid_matrix[1][2] == PointsEnum.SNAKE.value
+
+    def test_grid_0_0_and_1_2_positions_must_be_food_value(self):
+        grid = GridEntity(3, 3,
+                          foods_positions_list=[{"x": 0, "y": 0}, {'x': 1, 'y': 2}],
+                          snakes_positions_list=[],
+                          hazards_positions_list=[])
+
+        assert grid.rows == 3
+        assert grid.columns == 3
+        assert grid.grid_matrix[0][0] == PointsEnum.FOOD.value
+        assert grid.grid_matrix[1][2] == PointsEnum.FOOD.value
+
+    def test_grid_0_0_and_1_2_positions_must_be_hazard_value(self):
+        grid = GridEntity(3, 3,
+                          hazards_positions_list=[{"x": 0, "y": 0}, {'x': 1, 'y': 2}],
+                          snakes_positions_list=[],
+                          foods_positions_list=[])
+
+        assert grid.rows == 3
+        assert grid.columns == 3
+        assert grid.grid_matrix[0][0] == PointsEnum.HAZARD.value
+        assert grid.grid_matrix[1][2] == PointsEnum.HAZARD.value
