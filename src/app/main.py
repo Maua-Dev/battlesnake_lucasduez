a998b3ba84e1c66e72eac9270fafde204c4ffd9d
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-07-26 08:30:34 -0300

    update main file

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
+from .adapters.input_adapter_battle_snake import InputAdapterBattleSnake
+from .usecases.find_best_move_usecase import FindBestMoveUsecase
+from ..app.adapters.input_adapter_interface import IInputAdapter
+
 app = FastAPI()
 
 
@@ -17,8 +21,12 @@ def read_root():
 
 @app.post("/move")
 def move_snake(request: dict):
+    input_adapter = InputAdapterBattleSnake(request)
+    find_move_usecase = FindBestMoveUsecase(input_adapter, 9)
+    next_move = find_move_usecase.calculate_best_move()
+
     return {
-        "move": "right",
+        "move": next_move,
         "shout": "only right"
         }
 
