5e5faa784234f74a9751ce92f1cfe241696e48e2
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-07-26 08:14:07 -0300

    Add minimax algorhting on usecase

--- src/app/usecases/find_best_move_usecase.py
+++ src/app/usecases/find_best_move_usecase.py
@@ -1,23 +1,61 @@
+import math
+
 from ..adapters.input_adapter_interface import IInputAdapter
+from ..entities.grid import GridEntity
 from ..entities.movements_enum import Movements
+from ..entities.pontuation_enum import PointsEnum
+from ..entities.snake import SnakeEntity
 
 
 class FindBestMoveUsecase:
     input_adapter: IInputAdapter
+    maximum_turns_prediction: int
 
-    def __init__(self, input_adapter: IInputAdapter):
+    def __init__(self, input_adapter: IInputAdapter, max_turns_prediction: int):
         self.grid = input_adapter.grid
         self.my_snake = input_adapter.my_snake
+        self.maximum_turns_prediction = max_turns_prediction
+        self.all_snakes_list: [SnakeEntity] = input_adapter.all_snakes
 
     def calculate_best_move(self) -> Movements:
-        scores_dict = {
-            Movements.UP: self.minimax(Movements.UP),
-            Movements.DOWN: self.minimax(Movements.DOWN),
-            Movements.RIGHT: self.minimax(Movements.RIGHT),
-            Movements.LEFT: self.minimax(Movements.LEFT)
+        scores = {
+            Movements.UP: self.minimax(self.my_snake, Movements.UP),
+            Movements.DOWN: self.minimax(self.my_snake, Movements.DOWN),
+            Movements.RIGHT: self.minimax(self.my_snake, Movements.RIGHT),
+            Movements.LEFT: self.minimax(self.my_snake, Movements.LEFT)
         }
 
-        return max(scores_dict, scores_dict.get)
+        return max(scores, key=scores.get)
 
-    # def minimax(self, movement: Movements, turn: int = 1):
-    #     return 10
+    def minimax(self,snake: SnakeEntity, direction: Movements, turn: int = 1):
+        snake.move_snake(direction)
+
+        head_position = snake.head_pos
+        pos_value = self.grid.grid_matrix[head_position["x"]][head_position['y']]
+
+        if pos_value == PointsEnum.SNAKE.value:
+            return pos_value
+
+        if turn > self.maximum_turns_prediction:
+            return pos_value
+
+        self.grid.update_grid_values(self.all_snakes_list, self.grid.foods_positions_list, []) #Todo: estou ignorando a lista de comidas e de hazard
+        is_maximizing = turn % len(self.all_snakes_list) == 0
+        current_snake: SnakeEntity = self.all_snakes_list[(turn-1) % len(self.all_snakes_list)]
+
+        if not is_maximizing:
+            score_up = self.minimax(current_snake, Movements.UP, turn+1)
+            score_down = self.minimax(current_snake, Movements.DOWN, turn+1)
+            score_right = self.minimax(current_snake, Movements.RIGHT, turn+1)
+            score_left = self.minimax(current_snake, Movements.LEFT, turn+1)
+
+            return min(score_up, score_down, score_right, score_left)
+
+        if is_maximizing:
+            score_up = self.minimax(current_snake, Movements.UP, turn+1)
+            score_down = self.minimax(current_snake, Movements.DOWN, turn+1)
+            score_right = self.minimax(current_snake, Movements.RIGHT, turn+1)
+            score_left = self.minimax(current_snake, Movements.LEFT, turn+1)
+
+            return max(score_up, score_down, score_right, score_left)
+
---
+++ tests/app/usecases/test_find_best_move_usecase.py
@@ -0,0 +1,28 @@
+from src.app.adapters.input_adapter_mock import InputAdapterMock
+from src.app.entities.grid import GridEntity
+from src.app.entities.movements_enum import Movements
+from src.app.entities.snake import SnakeEntity
+from src.app.usecases.find_best_move_usecase import FindBestMoveUsecase
+
+input_adapter = InputAdapterMock({})
+grid = GridEntity(11, 11,
+                  snakes_list=[
+                      SnakeEntity(100, [{'x': 0, 'y': 0}], {'x': 0, 'y': 0}, 11, 11),
+                      SnakeEntity(100, [{'x': 10, 'y': 10}, {'x': 9, 'y': 10}], {'x': 10, 'y': 10}, 11, 11),
+                      # SnakeEntity(100, [{'x': 10, 'y': 0}, {'x': 10, 'y': 1}], {'x': 10, 'y': 0}, 11, 11)
+                  ],
+                  foods_positions_list=[],
+                  hazards_positions_list=[])
+
+input_adapter.grid = grid
+input_adapter.all_snakes = grid.snakes_list
+usecase = FindBestMoveUsecase(input_adapter, 2)
+
+
+class Test_FindBestMoveUsecase:
+
+    def test_kappa(self):
+        score = usecase.minimax(grid.snakes_list[-1], Movements.LEFT)
+
+
+        assert score == 0
